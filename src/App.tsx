f1bbd68643ac8ec62156cc56146e89ce189e6843
ortolahector <ortolahector@gmail.com>
2024-01-02 15:32:36 +0100

    Implemented night-mode partially.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import List from './components/List/List';
 import WineDetail from './components/WineDetail/WineDetail';
+import './App.css';
 
 const App: React.FC = () => {
+
+	const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  	const toggleDarkMode = () => {
+    	setIsDarkMode((prevMode) => !prevMode);
+  	};
+
 	return (
-		<Router>
-			<div>
-				<Routes>
-					<Route path="/" element={<List />} />
-					<Route path="/detail/:id" element={<WineDetail />} />
-				</Routes>
-			</div>
-		</Router>
+		<div className={`App ${isDarkMode ? 'dark-mode' : ''}`}>
+		<header className="App-header">
+		<button onClick={toggleDarkMode}>
+          {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+        </button>
+		</header>
+			<Router>
+				<div>
+					<Routes>
+						<Route path="/" element={<List />} />
+						<Route path="/detail/:id" element={<WineDetail />} />
+					</Routes>
+				</div>
+			</Router>
+		</div>
 	);
 };
 
